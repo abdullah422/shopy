7ee7b59bfa56859bc1d91036aace77d2694b4e88
abdullah <abdullahmouawad1999@gmail.com>
2022-08-01 03:53:48 +0200

    Initial commit

---
+++ lib/modules/login/cubit/states.dart
@@ -0,0 +1,23 @@
+
+import 'package:shopy/models/login_model.dart';
+
+abstract class LoginStates {}
+
+class ChangeRememberMeCheckBoxState extends LoginStates {}
+
+class ChangePasswordState extends LoginStates {}
+
+class LoginInitialState extends LoginStates {}
+
+class LoginLoadingState extends LoginStates {}
+
+class LoginSuccessState extends LoginStates {
+  final LoginModel loginModel;
+
+  LoginSuccessState(this.loginModel);
+}
+
+class LoginErrorState extends LoginStates {
+  final String error;
+  LoginErrorState(this.error);
+}
---
+++ lib/modules/register/register_cubit/register_states.dart
@@ -0,0 +1,20 @@
+
+import 'package:shopy/models/register_model.dart';
+
+class RegisterStates {}
+
+class InitialRegisterState extends RegisterStates {}
+
+class ChangeRememberMeCheckBoxState extends RegisterStates {}
+
+class ChangeRegisterPasswordState extends RegisterStates {}
+
+class RegisterLoadingState extends RegisterStates {}
+
+class RegisterSuccessState extends RegisterStates {
+  final RegisterModel registerModel;
+
+  RegisterSuccessState(this.registerModel);
+}
+
+class RegisterErrorState extends RegisterStates {}
